8b22ce4adb2982ff99001aff7a725ff522871953
George Caldarescu <caldarescu.george@gmail.com>
2021-09-04 16:47:53 +0200

    did it with widget

--- main.py
+++ main.py
@@ -4,48 +4,65 @@ from kivy.uix.label import Label  # to be able to add labels
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
 
 # create a class that contain all the desing elements
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs): # is used to take infinite arguments
-        super(MyGrid, self).__init__(**kwargs)
+# class MyGrid(GridLayout):
+#     def __init__(self, **kwargs): # is used to take infinite arguments
+#         super(MyGrid, self).__init__(**kwargs)
         
-        # create another layout inside the main layout to center the button
-        self.inside = GridLayout()
-        self.inside.cols = 2
-        self.cols = 1
+#         # create another layout inside the main layout to center the button
+#         self.inside = GridLayout()
+#         self.inside.cols = 2
+#         self.cols = 1
         
-        self.inside.add_widget(Label(text = "First Name: "))
-        self.firstName = TextInput(multiline = False)
-        self.inside.add_widget(self.firstName)
+#         self.inside.add_widget(Label(text = "First Name: "))
+#         self.firstName = TextInput(multiline = False)
+#         self.inside.add_widget(self.firstName)
 
-        self.inside.add_widget(Label(text = "Last Name: "))
-        self.lastName = TextInput(multiline = False)
-        self.inside.add_widget(self.lastName)
+#         self.inside.add_widget(Label(text = "Last Name: "))
+#         self.lastName = TextInput(multiline = False)
+#         self.inside.add_widget(self.lastName)
 
-        self.inside.add_widget(Label(text = "Email: "))
-        self.email = TextInput(multiline = False)
-        self.inside.add_widget(self.email)
+#         self.inside.add_widget(Label(text = "Email: "))
+#         self.email = TextInput(multiline = False)
+#         self.inside.add_widget(self.email)
 
-        self.add_widget(self.inside)
+#         self.add_widget(self.inside)
 
-        self.submit = Button(text = "Submit", font_size = 40)
-        self.submit.bind(on_press = self.pressed)
-        self.add_widget(self.submit)
+#         self.submit = Button(text = "Submit", font_size = 40)
+#         self.submit.bind(on_press = self.pressed)
+#         self.add_widget(self.submit)
     
     
-    def pressed(self, instance):    #this function is called when the button is pressed
-        name = self.firstName.text
-        last = self.lastName.text   # is how to store waht is inside the ui
-        email = self.email.text
+#     def pressed(self, instance):    #this function is called when the button is pressed
+#         name = self.firstName.text
+#         last = self.lastName.text   # is how to store waht is inside the ui
+#         email = self.email.text
 
-        print("name:", name, "last name:", last, "email:", email)
+#         print("name:", name, "last name:", last, "email:", email)
 
-        # to clean the form
-        self.firstName.text = ""
-        self.lastName.text = "" 
+#         # to clean the form
+#         self.firstName.text = ""
+#         self.lastName.text = "" 
+#         self.email.text = ""
+
+
+# try to have it in another file like css code
+class MyGrid(Widget):
+    
+
+    def btn(self):    #this function is called when the button is pressed
+        print("Name:", self.name.text, "email:", self.email.text)
+
+        # to clean the input
+        self.name.text = ""
         self.email.text = ""
+    
+
 
 class MyApp(App):
     def build(self):
