6bfa832b152d80272df357dc8f5ce113ec346403
George Caldarescu <caldarescu.george@gmail.com>
2021-09-04 22:56:20 +0200

    create the ui

--- dil.py
+++ dil.py
@@ -1,11 +1,30 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
+from kivy.lang import Builder  # this one is useful to load kv files whatever the name is
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class ThirdWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+ 
+
+
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return kv
 
 
 
