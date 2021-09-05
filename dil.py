2b209053e4169e558ee3dd62563448b314422001
George Caldarescu <caldarescu.george@gmail.com>
2021-09-05 15:06:26 +0200

    input in agro works. I need to finish the function

--- dil.py
+++ dil.py
@@ -3,13 +3,18 @@ from kivy.app import App
 from kivy.lang import Builder  # this one is useful to load kv files whatever the name is
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
 
 class MainWindow(Screen):
     pass
 
 class SecondWindow(Screen):
-    def btn():
-        print(self.AgroConc.text)
+  agro = ObjectProperty(None)
+  od = ObjectProperty(None)
+
+  def btn(self):
+      print("conc:" , type(float(self.agro.text)), "OD:",type(float(self.od.text)))
 
 class ThirdWindow(Screen):
     pass
@@ -19,11 +24,11 @@ class WindowManager(ScreenManager):
     pass
 
 kv = Builder.load_file("my.kv")
- 
 
 
 
 class MyApp(App):
+        
     def build(self):
         return kv
 
